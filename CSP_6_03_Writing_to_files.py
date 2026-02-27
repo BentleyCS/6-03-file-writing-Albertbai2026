28095962db6a170443ddbc737ce142877cdaf597
Albertbai2026 <abai26@bentleyschool.org>
2026-02-27 09:52:54 -0800

    Update CSP_6_03_Writing_to_files.py

--- CSP_6_03_Writing_to_files.py
+++ CSP_6_03_Writing_to_files.py
@@ -1,20 +1,17 @@
 import random
 
-# You need to create your own test file for this assignment.
-# Because we are dealing with both reading and writing to files. Your test file will be more complicated than it has been.
-
+#You need to create your own test file for this assignment.
+#Because we are dealing with both reading and writing to files. Your test file will be more complicated than it has been.
 
 def writeFile(inputList, fileName):
-    # Creates a file of the given name and adds each value from the list
-    # to the file, with each line being one item from the list.
+    # Creates a file and writes each list item on its own line
     with open(fileName, "w") as f:
-        for value in inputList:
-            f.write(str(value) + "\n")
+        for item in inputList:
+            f.write(str(item) + "\n")
 
 
 def sortNames(fileName, targetFile):
-    # Takes in a source file and a target file.
-    # Sort all of the values from the source file and write them to the target file.
+    # Reads names from source file, sorts, writes to target file
     with open(fileName, "r") as f:
         names = [line.strip() for line in f if line.strip()]
 
@@ -26,24 +23,20 @@ def sortNames(fileName, targetFile):
 
 
 def highScore(newScore: int):
-    # Adds a new score to the file scores.txt
-    # Then returns the average score from all of the scores in scores.txt
-    file_name = "scores.txt"
+    # Adds newScore to scores.txt, returns average of all scores
+    fileName = "scores.txt"
 
-    # append the new score
-    with open(file_name, "a") as f:
-        f.write(str(newScore) + "\n")
+    # create file if missing + append
+    with open(fileName, "a+") as f:
+        f.write(str(int(newScore)) + "\n")
 
-    # read back all scores and compute average
-    with open(file_name, "r") as f:
+    # read all scores
+    with open(fileName, "r") as f:
         scores = [int(line.strip()) for line in f if line.strip()]
 
-    if not scores:
-        return 0
-
-    return sum(scores) / len(scores)
+    return sum(scores) / len(scores) if scores else 0.0
 
 
-# Leave these calls if your teacher provided them
-sortNames("names.txt", "namesNew.txt")
-highScore(random.randint(1, 100))
+if __name__ == "__main__":
+    sortNames("names.txt", "namesNew.txt")
+    highScore(random.randint(1, 100))
